fb2b61ed292603686b8ed6763448eb794f2a3cd3
Maciek Rylko <rylko.maciej@gmail.com>
2017-08-23 18:50:31 +0200

    get boards details (names from cache)

--- lib/trello/members_details.ex
+++ lib/trello/members_details.ex
@@ -3,27 +3,26 @@ defmodule Trello.MembersDetails do
   @resource_path "members/"
 
   def get_names(members_ids, fetch_method \\ &Trello.Fetcher.fetch/2) do
-    Enum.map(members_ids, fn id -> get_name(id, %{}, fetch_method) end)
+    Enum.map(members_ids, fn id -> get_name(id, fetch_method) end)
     |> Enum.filter(fn name -> name != nil end)
   end
 
-  defp get_name(member_id, cache, fetch_method \\ &Trello.Fetcher.fetch/2) do
-    Map.get(cache, member_id, nil)
-    |> fetch_value(member_id, cache, fetch_method)
+  defp get_name(member_id, fetch_method \\ &Trello.Fetcher.fetch/2) do
+    Trello.NamesCache.get_value(member_id)
+    |> fetch_value(member_id, fetch_method)
   end
 
-  defp fetch_value(nil, member_id, cache, fetch_method) do
+  defp fetch_value(nil, member_id, fetch_method) do
     full_name = "#{@resource_path}#{member_id}"
     |> fetch_method.(@resource_params)
     |> handle_response
 
-    cache_new = Map.put(cache, member_id, full_name)
-    IO.inspect cache_new
+    Trello.NamesCache.save_value({member_id, full_name})
+
     full_name
   end
 
   defp fetch_value(full_name, _, _) do
-    IO.puts "Get from cache #{full_name}"
     full_name
   end
 
